3496fba9fa07fd7727d26592b179af0c8131aa0e
Jiyoon Ha <jyi01055@gmail.com>
2025-02-27 16:16:36 +0900

    gitignore file setting

---
+++ Beakjoon/src/Search/n1012.java
@@ -0,0 +1,10 @@
+package Search;
+
+public class n1012 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Beakjoon/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author User
+ *
+ */
+module Back_joon {
+}
